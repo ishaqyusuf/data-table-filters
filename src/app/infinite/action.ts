93dd3345f2898fc1bfc8aa94d9d40f0004270373
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2024-11-15 10:11:13 +0100

    :tada:

--- src/app/infinite/action.ts
+++ src/app/infinite/action.ts
@@ -5,6 +5,7 @@ import { searchParamsCache } from "./search-params";
 import { mock } from "./api/mock";
 import { filterData, percentileData, sortData } from "./api/helpers";
 import { unstable_noStore } from "next/cache";
+import { action2 } from "./action2";
 
 export async function __fetch(search: any) {
   //   const search = searchParamsCache.parse(query);
@@ -12,48 +13,4 @@ export async function __fetch(search: any) {
   // await new Promise((resolve) => setTimeout(resolve, 500));
   unstable_noStore();
   return await action2(search);
-}
-async function action2(search: any) {
-  const totalData = mock;
-  const filteredData = filterData(totalData, search);
-  const sortedData = sortData(filteredData, search.sort);
-  const withPercentileData = percentileData(sortedData);
-
-  // FIXME: this is fugly
-  const totalFilters = totalData.reduce((prev, curr) => {
-    for (const key in curr) {
-      const value = curr[key as keyof typeof curr];
-      const prevValue = prev[key as keyof typeof prev] || [];
-      if (Array.isArray(value)) {
-        prev[key as keyof typeof prev] = [
-          // @ts-ignore
-          ...new Set([...prevValue, ...value]),
-        ];
-      } else {
-        // @ts-ignore
-        prev[key as keyof typeof prev] = [...new Set([...prevValue, value])];
-      }
-    }
-    return prev;
-  }, {} as Record<string, (number | string | boolean | Date)[]>);
-
-  const latencies = withPercentileData.map(({ latency }) => latency);
-
-  const currentPercentiles = {
-    50: calculateSpecificPercentile(latencies, 50),
-    75: calculateSpecificPercentile(latencies, 75),
-    90: calculateSpecificPercentile(latencies, 90),
-    95: calculateSpecificPercentile(latencies, 95),
-    99: calculateSpecificPercentile(latencies, 99),
-  };
-
-  return {
-    data: withPercentileData.slice(search.start, search.start + search.size),
-    meta: {
-      totalRowCount: totalData.length,
-      filterRowCount: filteredData.length,
-      totalFilters,
-      currentPercentiles,
-    },
-  };
 }
---
+++ src/app/infinite/action2.ts
@@ -0,0 +1,48 @@
+import { calculateSpecificPercentile } from "@/lib/request/percentile";
+import { searchParamsCache } from "./search-params";
+import { mock } from "./api/mock";
+import { filterData, percentileData, sortData } from "./api/helpers";
+export async function action2(search: any) {
+  const totalData = mock;
+  const filteredData = filterData(totalData, search);
+  const sortedData = sortData(filteredData, search.sort);
+  const withPercentileData = percentileData(sortedData);
+
+  // FIXME: this is fugly
+  const totalFilters = totalData.reduce((prev, curr) => {
+    for (const key in curr) {
+      const value = curr[key as keyof typeof curr];
+      const prevValue = prev[key as keyof typeof prev] || [];
+      if (Array.isArray(value)) {
+        prev[key as keyof typeof prev] = [
+          // @ts-ignore
+          ...new Set([...prevValue, ...value]),
+        ];
+      } else {
+        // @ts-ignore
+        prev[key as keyof typeof prev] = [...new Set([...prevValue, value])];
+      }
+    }
+    return prev;
+  }, {} as Record<string, (number | string | boolean | Date)[]>);
+
+  const latencies = withPercentileData.map(({ latency }) => latency);
+
+  const currentPercentiles = {
+    50: calculateSpecificPercentile(latencies, 50),
+    75: calculateSpecificPercentile(latencies, 75),
+    90: calculateSpecificPercentile(latencies, 90),
+    95: calculateSpecificPercentile(latencies, 95),
+    99: calculateSpecificPercentile(latencies, 99),
+  };
+
+  return {
+    data: withPercentileData.slice(search.start, search.start + search.size),
+    meta: {
+      totalRowCount: totalData.length,
+      filterRowCount: filteredData.length,
+      totalFilters,
+      currentPercentiles,
+    },
+  };
+}
