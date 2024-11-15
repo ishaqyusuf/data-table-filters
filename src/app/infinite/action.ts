60653210a4a8398a80735ad2b51ce84f57b66492
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2024-11-15 09:31:17 +0100

    :tada:

--- src/app/infinite/action.ts
+++ src/app/infinite/action.ts
@@ -4,12 +4,13 @@ import { calculateSpecificPercentile } from "@/lib/request/percentile";
 import { searchParamsCache } from "./search-params";
 import { mock } from "./api/mock";
 import { filterData, percentileData, sortData } from "./api/helpers";
+import { unstable_noStore } from "next/cache";
 
 export async function __fetch(search: any) {
   //   const search = searchParamsCache.parse(query);
   // Simulate a database query
   // await new Promise((resolve) => setTimeout(resolve, 500));
-
+  unstable_noStore();
   const totalData = mock;
   const filteredData = filterData(totalData, search);
   const sortedData = sortData(filteredData, search.sort);
